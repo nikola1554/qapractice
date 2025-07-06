f9a0990aa4846c9b4f45410716d270e81dd4315b
Mykola Sakhatskyi <kotlenoko54@gmail.com>
2025-07-06 20:40:47 +0300

    modified conftest.py

--- conftest.py
+++ conftest.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 import pytest
 # from selenium.webdriver.chrome.options import Options  # with this import chrom doesn't work in headless mode
-from selenium.webdriver.firefox.options import Options
+#from selenium.webdriver.firefox.options import Options
 
 @pytest.fixture()
 def browser():
-    options = Options()
-    options.add_argument('--headless')
-    browser = webdriver.Firefox(options=options)
+    # options = Options()
+    # options.add_argument('--headless')
+    # browser = webdriver.Firefox(options=options)
+    browser = webdriver.Firefox()
     browser.implicitly_wait(2)
     browser.maximize_window()
-    return browser
+    yield browser
+    browser.close()
