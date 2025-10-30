565c842c3d4b41f29573ad2edecb958ed9078077
Mykola Sakhatskyi <kotletoko54@gmail.com>
2025-10-30 15:59:30 +0200

    some chamges

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 import pytest
-# from selenium.webdriver.chrome.options import Options  # with this import chrom doesn't work in headless mode
 from selenium.webdriver.firefox.options import Options
 
 @pytest.fixture()
